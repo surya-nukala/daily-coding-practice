361244e4b5a7983054755331647aa61838878b3f
surya-nukala <nukalajayasurya@gmail.com>
2026-08-26 17:27:06 +0530

    Update MostWordsFound solution

--- Python/Arrays/Strings/MostWordsFound.py
+++ Python/Arrays/Strings/MostWordsFound.py
@@ -1,8 +1,8 @@
 class Solution:
     def mostWordsFound(self, sentences: List[str]) -> int:
         c = []
-
         for sentence in sentences:
             c.append(len(sentence.split()))
-
-        return max(c)
+        return max(c)\
+# Time complexity: O(n*m) where n is the number of sentences and m is the average length of the sentences
+# Space complexity: O(n) where n is the number of sentences
